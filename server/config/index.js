4b8fd4517128cd22a0a0967546f6e8e255fa88e7
azakic <wolken@its.rs>
2022-04-24 01:31:44 +0200

    mongodb

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+    db: {
+      url: 'leylixchat:iIoDEnajszYvmFkR@cluster0.oa13d.mongodb.net',//leylixchat?retryWrites=true&w=majority',
+      name: 'leylixchat'
+    }
+  }
+  
+  export default config
